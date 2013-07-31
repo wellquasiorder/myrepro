8d72034449f2312b4b8e05bd1731b5e380a40d0e
wellquasiorder <lin@nii.ac.jp>
2013-07-31 06:52:57 +0000

    add files

---
+++ web.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var infile = "index.html";
+var fs=require('fs');
+app.use(express.logger());
+  
+app.get('/', function(request, response) {
+ var buffer=fs.readFileSync(infile);
+response.send(buffer.toString('utf-8'));
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
